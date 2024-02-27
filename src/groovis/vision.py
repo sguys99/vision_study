0b2f73ecda2ac05841053364a3ce2acd17576087
Kwang Myung Yu <sguys99@gmail.com>
2024-02-27 21:22:33 +0900

    :sparkles: 4-2 시각의 품질 평가
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

--- src/groovis/vision.py
+++ src/groovis/vision.py
@@ -1,4 +1,12 @@
 from PIL import Image
+from einops import reduce
+import torch
 
-def vision(image: Image.Image):
-    return 'ok'
+
+PATCH_SIZE = 16
+
+
+def vision(image: torch.Tensor) -> torch.Tensor:
+    return reduce(
+        image, "c (h ph) (w pw) ->h w", "mean", ph=PATCH_SIZE, pw=PATCH_SIZE
+    )
--- tests/test_vision.py
+++ tests/test_vision.py
@@ -1,19 +1,26 @@
 from PIL import Image
+import torch.nn.functional as F
+
 from groovis import vision
+from groovis.utils import image_path_to_tensor
 
 
 def test_invariance():
-    image_tiger_1 = Image.open("tests/images/tiger1.webp")
-    image_tiger_2 = Image.open("tests/images/tiger2.webp")
-    image_dog = Image.open("tests/images/dog.webp")
+    image_tiger_1 = image_path_to_tensor("tests/images/tiger1.webp")
+    image_tiger_2 = image_path_to_tensor("tests/images/tiger2.webp")
+    image_dog = image_path_to_tensor("tests/images/dog.webp")
     
     tiger_1 = vision(image_tiger_1)
     tiger_2 = vision(image_tiger_2)
     dog = vision(image_dog)
     
-    diff_tiger_tiger = tiger_2 - tiger_2
-    diff_tiger_dog_1 = tiger_1 - dog
-    diff_tiger_dog_2 = tiger_2 - dog
+    diff_tiger_tiger = F.l1_loss(tiger_2, tiger_2)# 차의 절대값의 평균
+    diff_tiger_dog_1 = F.l1_loss(tiger_1, dog)
+    diff_tiger_dog_2 = F.l1_loss(tiger_2, dog)
     
-    assert (diff_tiger_dog_1 - diff_tiger_dog_2) / 2 > diff_tiger_tiger
+    quality = (diff_tiger_dog_1 * diff_tiger_dog_2) / 2 - diff_tiger_tiger
+    
+    print(quality)
+    
+    assert quality > 0
     
