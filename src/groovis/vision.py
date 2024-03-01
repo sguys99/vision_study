4b01f29fc6ee6359b671e6b6f558614a116b675a
Kwang Myung Yu <sguys99@gmail.com>
2024-03-01 11:55:14 +0900

    :sparkles: 4-7 배치 처리
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

--- src/groovis/vision.py
+++ src/groovis/vision.py
@@ -8,21 +8,21 @@ PATCH_SIZE = 16
 
 class Vision:
     def __init__(self) -> None:
-        self.weight = torch.randn(1024, 3, 16, 16) / (3*16*16)
+        self.weight = torch.randn(1024, 3*16*16) / (3*16*16)
         
     def __call__(self, image: torch.Tensor) -> torch.Tensor:
 
         patches = rearrange(
             image, 
-            "c (h ph) (w pw) -> h w c ph pw", 
+            "b c (h ph) (w pw) -> b (h w) (c ph pw)",
             ph=PATCH_SIZE, 
             pw=PATCH_SIZE
         )
         
         representation = torch.einsum(
-            "i j c h w, d c h w ->i j d", patches, self.weight
+            "b n p, d p -> b n d", patches, self.weight
             )
         
-        representation = reduce(representation, "h w d -> d", "mean")
+        representation = reduce(representation, "b n d -> b d", "mean")
         
         return representation
